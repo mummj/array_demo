20d6335dc2375b78245a9eebb7b09b41ef71871c
mummj <68560201+mummj@users.noreply.github.com>
2022-01-20 15:09:22 -0800

    added js

---
+++ source.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const pet =['dog','cat','fish'];
+
+pet.push('lizzard');
+
+pet.push('hamster');
+
+pet.pop('hamster');
+
+pet.shift('dog');
+
+console.log(pet.length);
+console.log(pet[i]);
+console.log(pet[0]);
+
